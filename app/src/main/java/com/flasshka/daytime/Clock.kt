380b85a466ab20fd690c600365f4986e3072bf36
Flash-Kaa <78853080+Flash-Kaa@users.noreply.github.com>
2023-07-14 15:27:34 +0500

    add clock animation

--- app/src/main/java/com/flasshka/daytime/Clock.kt
+++ app/src/main/java/com/flasshka/daytime/Clock.kt
@@ -1,50 +1,49 @@
-import android.os.Build
-import android.os.SystemClock
 import android.util.Log
-import android.widget.TextClock
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import com.jakewharton.threetenabp.AndroidThreeTen
+import androidx.compose.ui.unit.*
 import kotlinx.coroutines.*
 import org.threeten.bp.LocalTime
 import org.threeten.bp.format.DateTimeFormatter
-import java.time.Clock
-import kotlin.concurrent.thread
-
 
 class Clock {
-
-    private val timeToWriteState = mutableStateOf("")
-
-    var timeToWrite: String
-        get() = timeToWriteState.value
+    private val timeState = mutableStateOf(LocalTime.now())
+    var time: LocalTime
+        get() = timeState.value
         private set(value) {
-            timeToWriteState.value = value
+            timeState.value = value
         }
+
+    val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
 
     init {
         timerUpd()
     }
 
+    private fun timerUpd() {
+        MainScope().launch {
+            while (true) {
+                time = LocalTime.now()
+                Log.d("test", time.format(timeFormatter))
+                delay(100)
+            }
+        }
+    }
+
     @Composable
     fun DrawTimer(
-        modifier: Modifier,
-        color: Color,
-        width: Dp,
-        fontSize: TextUnit
+        modifier: Modifier = Modifier,
+        fontSize: TextUnit = 35.sp,
+        widthOfSecondArc: Dp = 15.dp,
+        secondArcColors: List<Color> = listOf(Color.Blue, Color.Green),
     ) {
         Box(
             modifier = modifier,
@@ -52,37 +51,20 @@ class Clock {
         ) {
             Canvas(modifier = modifier
             ) {
-
                 drawArc(
-                    color = color,
+                    brush = Brush.horizontalGradient(
+                        colors = secondArcColors
+                    ),
                     startAngle = -90f,
-                    sweepAngle = 134f,
+                    sweepAngle = (timeState.value.nano * 0.000000001f + timeState.value.second) * 6f,
                     useCenter = false,
                     style = Stroke (
-                        width = width.toPx(),
+                        width = widthOfSecondArc.toPx(),
                         cap = StrokeCap.Round
                     )
                 )
             }
-            Text(text = timeToWrite, fontSize = fontSize)
-            Log.d("test", "text written")
-        }
-    }
-
-    fun timerUpd() {
-        val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
-        Log.d("test", "formatter init")
-
-        MainScope().launch {
-            while (true) {
-                val time = LocalTime.now().format(formatter)
-
-                if(time != timeToWrite) {
-                    Log.d("test", time)
-                    timeToWrite = time
-                }
-                delay(50)
-            }
+            Text(text = time.format(timeFormatter), fontSize = fontSize)
         }
     }
 }
--- app/src/main/java/com/flasshka/daytime/MainActivity.kt
+++ app/src/main/java/com/flasshka/daytime/MainActivity.kt
@@ -6,14 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.jakewharton.threetenabp.AndroidThreeTen
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,26 +18,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidThreeTen.init(this)
             val clock = Clock()
-            MainPage(clock)
+
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+
+            ) {
+                clock.DrawTimer(
+                    modifier = Modifier.size(200.dp)
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun MainPage(clock: Clock) {
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Green),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-
-    ) {
-        clock.DrawTimer(
-            modifier = Modifier.size(200.dp),
-            color = Color.Black,
-            width = 10.dp,
-            fontSize = 25.sp
-        )
     }
 }
